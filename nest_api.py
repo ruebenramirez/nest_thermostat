c4514321aabe6147f36679eb8ac17ef8ebfb2a39
Rueben Ramirez <ruebenramirez@gmail.com>
2015-01-04 22:48:47 +0000

    changing root route to nest status

--- nest_api.py
+++ nest_api.py
@@ -2,11 +2,31 @@
 
 from flask import Flask
 app = Flask(__name__)
+import ConfigParser
 from nest_thermostat import Nest
 
+
+class Config(object):
+
+    def __init__(self):
+        conf_file = 'nest.cfg'
+        conf = ConfigParser.ConfigParser()
+        conf.readfp(open(conf_file))
+        self.user = str(conf.get('Nest', 'username')))
+        self.password = str(conf.get('Nest', 'password'))
+        self.serial = str(conf.get('Nest', 'Serial'))
+        self.index = str(conf.get('Nest', 'Index'))
+
+def get_nest():
+    opts = Config()
+    n = Nest(opts.user, opts.password, opts.serial, opts.index, units=units)
+    n.login()
+    return n
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def nest_status():
+    n = get_nest()
+    return n.get_status()
 
 
 if __name__ == '__main__':
