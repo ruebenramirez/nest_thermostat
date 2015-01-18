5f603d0be65d78eb9d4ee67575d15019061bd063
Rueben Ramirez <ruebenramirez@gmail.com>
2015-01-18 09:40:34 +0000

    changing default run port to 80 for now

--- run_docker.sh
+++ run_docker.sh
@@ -7,7 +7,7 @@ sudo docker build -t nest-api .
 sudo docker ps -a | grep nest-api | grep -v 'CONTAINER' | awk '{print $1}' | xargs sudo docker rm -f
 
 sudo docker run -d \
-    -p 5000:5000 \
+    -p 80:5000 \
     -v $WORKING_DIR:/nest-api \
     --name nest-api \
     nest-api
